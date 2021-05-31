d5f43d4d5ee839b50b5286650fba9487e3d75993
Azaka <>
2021-05-31 05:13:55 +0000

    adding js file

---
+++ app.js
@@ -0,0 +1,11 @@
+  alert("welcome to our website! ");
+
+ var favAnime = prompt ('what is your favourate anime?');
+ if (favAnime === 'Naruto') {
+  document.write("watch Naruto" )
+ }
+ 
+else {
+document.write("watch aot");}
+
+
